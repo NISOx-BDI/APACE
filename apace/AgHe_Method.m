a10d980b730f9dd0aea6b36ad2290d1f9aa3add4
nicholst <thomas.nichols@bdi.ox.ac.uk>
2022-02-16 21:26:02 +0000

    Minimal changes

--- apace/AgHe_Method.m
+++ apace/AgHe_Method.m
@@ -122,14 +122,13 @@ SibPindex = [MZFindex; SibFindex];
 nSibP = size(SibPindex,1);
 
 % Generate permutation and bootstrapping labels for rDZ & rSib
-if ( nDZF==0 || nSibP==0 )
-    error('Cannot make permutation inference as there are no DZ twins or sibling pairs!!')
-end
-if ACEfit_Par.nPerm>0
-    [Perm_DZSib] = CreatePerm(nDZF,nSibP,ACEfit_Par.nPerm);
-end
-if ACEfit_Par.nBoot>0
-    [Boot_DZSib] = CreateBoot(nDZF,nSibP,0,ACEfit_Par.nBoot);
+if nSibP>0
+    if ACEfit_Par.nPerm>0
+        [Perm_DZSib] = CreatePerm(nDZF,nSibP,ACEfit_Par.nPerm);
+    end
+    if ACEfit_Par.nBoot>0
+        [Boot_DZSib] = CreateBoot(nDZF,nSibP,0,ACEfit_Par.nBoot);
+    end
 end
 
 
@@ -139,32 +138,34 @@ end
 ResY1 = [Res(cFam(1:nMZF+nDZF),:);   Res(SibPindex(:,1),:)];
 ResY2 = [Res(cFam(1:nMZF+nDZF)+1,:); Res(SibPindex(:,2),:)];
 
-rMZo  = zeros(nMZF,1);
-rDZo  = zeros(nDZF,1);
-rSibo = zeros(nSibP,1);
-
+rMZo = zeros(nMZF,1);
 for j=1:nMZF
-    MZ1      = ResY1(j,:);
-    MZ2      = ResY2(j,:);
-    rMZ      = corrcoef(MZ1,MZ2);
-    rMZo(j)  = rMZ(1,2);
+    MZ1     = ResY1(j,:);
+    MZ2     = ResY2(j,:);
+    rMZ     = corrcoef(MZ1,MZ2);
+    rMZo(j) = rMZ(1,2);
 end
+rDZo = zeros(nDZF,1);
 for j=1:nDZF
-    DZ1      = ResY1(nMZF+j,:);
-    DZ2      = ResY2(nMZF+j,:);
-    rDZ      = corrcoef(DZ1,DZ2);
-    rDZo(j)  = rDZ(1,2);
+    DZ1     = ResY1(nMZF+j,:);
+    DZ2     = ResY2(nMZF+j,:);
+    rDZ     = corrcoef(DZ1,DZ2);
+    rDZo(j) = rDZ(1,2);
 end
-for j=1:nSibP
-    Sib1     = ResY1(nMZF+nDZF+j,:);
-    Sib2     = ResY2(nMZF+nDZF+j,:);
-    rSib     = corrcoef(Sib1,Sib2);
-    rSibo(j) = rSib(1,2);
-end
+% Point estimates of rMZ-rDZ
+Est_MZDZ = mean(rMZo) - mean(rDZo);
 
-% Point estimates of rMZ-rDZ & rDZ-rSib
-Est_MZDZ  = mean(rMZo) - mean(rDZo);
-Est_DZSib = mean(rDZo) - mean(rSibo);
+if nSibP>0
+    rSibo = zeros(nSibP,1);
+    for j=1:nSibP
+        Sib1     = ResY1(nMZF+nDZF+j,:);
+        Sib2     = ResY2(nMZF+nDZF+j,:);
+        rSib     = corrcoef(Sib1,Sib2);
+        rSibo(j) = rSib(1,2);
+    end
+    % Point estimates of rDZ-rSib
+    Est_DZSib = mean(rDZo) - mean(rSibo);
+end
 
 
 f = get(0,'Children');
@@ -177,20 +178,31 @@ end
 
 SetFig(f);
 % AgHe box plots
-Z = [rMZo;                  rDZo;                 rSibo                  ];
-g = [zeros(length(rMZo),1); ones(length(rDZo),1); 2*ones(length(rSibo),1)];
-boxplot(Z,g);
-title(sprintf('Boxplots of Correlations of Aggregate Heritabilities'));
-set(gca, 'XTick', [1; 2; 3]);
-set(gca, 'XTickLabel', {'MZ'; 'DZ'; 'SIB'});
-print('-dpdf',fullfile(ACEfit_Par.ResDir,'AgHe_CorrPlots.pdf'));
-
-
-
-
-save(fullfile(ACEfit_Par.ResDir,['Ests_AgHe' ResSuf]),...
-                                 'Est_MZDZ','Est_DZSib',...
-                                 'rMZo', 'rDZo', 'rSibo');
+if nSibP>0
+    Z = [rMZo;                  rDZo;                 rSibo                  ];
+    g = [zeros(length(rMZo),1); ones(length(rDZo),1); 2*ones(length(rSibo),1)];
+    boxplot(Z,g);
+    title(sprintf('Boxplots of Correlations of Aggregate Heritabilities'));
+    set(gca, 'XTick', [1; 2; 3]);
+    set(gca, 'XTickLabel', {'MZ'; 'DZ'; 'SIB'});
+    print('-dpdf',fullfile(ACEfit_Par.ResDir,'AgHe_CorrPlots.pdf'));
+    
+    save(fullfile(ACEfit_Par.ResDir,['Ests_AgHe' ResSuf]),...
+                                     'Est_MZDZ','Est_DZSib',...
+                                     'rMZo', 'rDZo', 'rSibo');
+else
+    Z = [rMZo;                  rDZo;               ];
+    g = [zeros(length(rMZo),1); ones(length(rDZo),1)];
+    boxplot(Z,g);
+    title(sprintf('Boxplots of Correlations of Aggregate Heritabilities'));
+    set(gca, 'XTick', [1; 2]);
+    set(gca, 'XTickLabel', {'MZ'; 'DZ'});
+    print('-dpdf',fullfile(ACEfit_Par.ResDir,'AgHe_CorrPlots.pdf'));
+    
+    save(fullfile(ACEfit_Par.ResDir,['Ests_AgHe' ResSuf]),...
+                                     'Est_MZDZ',...
+                                     'rMZo', 'rDZo');
+end
 
 
 %
@@ -201,17 +213,11 @@ if ACEfit_Par.nPerm>0
     
     N = ACEfit_Par.nPerm+1;
     
-    corrMZ1  = zeros(N,nMZF);
-    corrDZ1  = zeros(N,nDZF);
+    corrMZ1 = zeros(N,nMZF);
+    corrDZ1 = zeros(N,nDZF);
     
-    corrDZ2  = zeros(N,nDZF);
-    corrSib2 = zeros(N,nSibP);
-    
-    corrMZ1(end,:)  = rMZo;
-    corrDZ1(end,:)  = rDZo;
-    
-    corrDZ2(end,:)  = rDZo;
-    corrSib2(end,:) = rSibo;
+    corrMZ1(end,:) = rMZo;
+    corrDZ1(end,:) = rDZo;
     
     % Compute rMZ & rDZ for all permutations
     for i=1:ACEfit_Par.nPerm
@@ -222,22 +228,37 @@ if ACEfit_Par.nPerm>0
         corrDZ1(i,:)  = corrMZDZ(nMZF+[1:nDZF]);
     end
     
-    % Compute rDZ & rSib for all permutations
-    for i=1:ACEfit_Par.nPerm
-        Perm_label    = Perm_DZSib(i,:);
-        corrDZSib     = [corrDZ2(end,:) corrSib2(end,:)];
-        corrDZSib     = corrDZSib(Perm_label);
-        corrDZ2(i,:)  = corrDZSib(1:nDZF);
-        corrSib2(i,:) = corrDZSib(nDZF+[1:nSibP]);
+    % 1st MZ vs DZ statistic: 2-sample t-statistic
+    t_stats_MZ_DZ  = (mean(corrMZ1,2)-mean(corrDZ1,2))./sqrt(var(corrMZ1,0,2)/nMZF+var(corrDZ1,0,2)/nDZF);
+    
+    % 2nd MZ vs DZ statistic: mean difference
+    CorrDiff_MZ_DZ = mean(corrMZ1,2)-mean(corrDZ1,2);
+    
+
+    if nSibP>0
+        
+        corrDZ2  = zeros(N,nDZF);
+        corrSib2 = zeros(N,nSibP);
+        
+        corrDZ2(end,:)  = rDZo;
+        corrSib2(end,:) = rSibo;
+        
+        % Compute rDZ & rSib for all permutations
+        for i=1:ACEfit_Par.nPerm
+            Perm_label    = Perm_DZSib(i,:);
+            corrDZSib     = [corrDZ2(end,:) corrSib2(end,:)];
+            corrDZSib     = corrDZSib(Perm_label);
+            corrDZ2(i,:)  = corrDZSib(1:nDZF);
+            corrSib2(i,:) = corrDZSib(nDZF+[1:nSibP]);
+        end
+        
+        % 1st DZ vs Sib statistic: 2-sample t-statistic
+        t_stats_DZ_Sib  = (mean(corrDZ2,2)-mean(corrSib2,2))./sqrt(var(corrDZ2,0,2)/nDZF+var(corrSib2,0,2)/nSibP);
+        
+        % 2nd DZ vs Sib statistic: mean difference
+        CorrDiff_DZ_Sib = mean(corrDZ2,2)-mean(corrSib2,2);
+        
     end
-    
-    % 1st statistic: 2-sample t-statistic
-    t_stats_MZ_DZ   = (mean(corrMZ1,2)-mean(corrDZ1,2))./sqrt(var(corrMZ1,0,2)/nMZF+var(corrDZ1,0,2)/nDZF);
-    t_stats_DZ_Sib  = (mean(corrDZ2,2)-mean(corrSib2,2))./sqrt(var(corrDZ2,0,2)/nDZF+var(corrSib2,0,2)/nSibP);
-    
-    % 2nd statistic: mean difference
-    CorrDiff_MZ_DZ  = mean(corrMZ1,2)-mean(corrDZ1,2);
-    CorrDiff_DZ_Sib = mean(corrDZ2,2)-mean(corrSib2,2);
     
     
     %
@@ -245,10 +266,8 @@ if ACEfit_Par.nPerm>0
     %
     ctl_val_index = N-floor(N*Palpha);
     
-    f = 0;
-    
     %
-    % 1st statistic: 2-sample t-statistic
+    % 1st MZ vs DZ statistic: 2-sample t-statistic
     %
     f = f+1;
     SetFig(f);
@@ -271,31 +290,8 @@ if ACEfit_Par.nPerm>0
     text(ctl_val_t_stats_MZ_DZ,0.9*yLimits(2),num2str(ctl_val_t_stats_MZ_DZ))
     print('-dpdf',fullfile(ACEfit_Par.ResDir,['H0dist_Tstat_rMZ_rDZ' ResSuf '.pdf']));
     
-    
-    f = f+1;
-    SetFig(f);
-    [st_stats_DZ_Sib,~]    = sort(t_stats_DZ_Sib);
-    ctl_val_t_stats_DZ_Sib = st_stats_DZ_Sib(ctl_val_index);
-    [f1,x1]                = hist(st_stats_DZ_Sib,100);
-    n_t_stats_DZ_Sib       = min(find(st_stats_DZ_Sib(:)==t_stats_DZ_Sib(N)));
-    bar(x1,f1/N);
-    
-    % Calculate the permutation-based p-value
-    p_t_stats_DZ_Sib = (N-n_t_stats_DZ_Sib+1)/N;
-    
-    title(sprintf('H0 dist of two-sample t-statistic for rDZ & rSib, P-value=%.3f',p_t_stats_DZ_Sib));
-    xlabel('t-statistic for rDZ vs. rSib');
-    ylabel('frequency');
-    yLimits = get(gca,'YLim');
-    line([t_stats_DZ_Sib(N) t_stats_DZ_Sib(N)],[0 yLimits(2)],'Marker','.','Color','green');
-    line([ctl_val_t_stats_DZ_Sib ctl_val_t_stats_DZ_Sib],[0 yLimits(2)],'Marker','.','LineStyle','-.','Color','red');
-    text(t_stats_DZ_Sib(N),0.8*yLimits(2),num2str(t_stats_DZ_Sib(N)))
-    text(ctl_val_t_stats_DZ_Sib,0.9*yLimits(2),num2str(ctl_val_t_stats_DZ_Sib))
-    print('-dpdf',fullfile(ACEfit_Par.ResDir,['H0dist_Tstat_rDZ_rSib' ResSuf '.pdf']));
-    
-    
     %
-    % 2nd statistic: mean difference
+    % 2nd MZ vs DZ statistic: mean difference
     %
     f = f+1;
     SetFig(f);
@@ -318,33 +314,66 @@ if ACEfit_Par.nPerm>0
     text(ctl_val_CorrDiff_MZ_DZ,0.9*yLimits(2),num2str(ctl_val_CorrDiff_MZ_DZ))
     print('-dpdf',fullfile(ACEfit_Par.ResDir,['H0dist_Diff_rMZ_rDZ' ResSuf '.pdf']));
     
-    
-    f = f+1;
-    SetFig(f);
-    [sCorrDiff_DZ_Sib,~]    = sort(CorrDiff_DZ_Sib);
-    ctl_val_CorrDiff_DZ_Sib = sCorrDiff_DZ_Sib(ctl_val_index);
-    [f2,x2]                 = hist(sCorrDiff_DZ_Sib,100);
-    n_CorrDiff_DZ_Sib       = min(find(sCorrDiff_DZ_Sib(:)==CorrDiff_DZ_Sib(N)));
-    bar(x2,f2/N);
-    
-    % Calculate the permutation-based p-value
-    p_CorrDiff_DZ_Sib = (N-n_CorrDiff_DZ_Sib+1)/N;
-    
-    title(sprintf('H0 dist of mean difference between rDZ & rSib, P-value=%.3f',p_CorrDiff_DZ_Sib));
-    xlabel('E[rDZ-rSib]');
-    ylabel('frequency');
-    yLimits = get(gca,'YLim');
-    line([CorrDiff_DZ_Sib(N) CorrDiff_DZ_Sib(N)],[0 yLimits(2)],'Marker','.','Color','green');
-    line([ctl_val_CorrDiff_DZ_Sib ctl_val_CorrDiff_DZ_Sib],[0 yLimits(2)],'Marker','.','LineStyle','-.','Color','red');
-    text(CorrDiff_DZ_Sib(N),0.8*yLimits(2),num2str(CorrDiff_DZ_Sib(N)))
-    text(ctl_val_CorrDiff_DZ_Sib,0.9*yLimits(2),num2str(ctl_val_CorrDiff_DZ_Sib))
-    print('-dpdf',fullfile(ACEfit_Par.ResDir,['H0dist_Diff_rDZ_rSib' ResSuf '.pdf']));
-    
-    
     Pvals_MZDZ  = [p_t_stats_MZ_DZ  p_CorrDiff_MZ_DZ]';
-    Pvals_DZSib = [p_t_stats_DZ_Sib p_CorrDiff_DZ_Sib]';
     
-    save(fullfile(ACEfit_Par.ResDir,['Pvals_AgHe' ResSuf]),'Pvals_MZDZ','Pvals_DZSib');
+    
+    if nSibP>0
+        
+        %
+        % 1st DZ vs Sib statistic: 2-sample t-statistic
+        %
+        f = f+1;
+        SetFig(f);
+        [st_stats_DZ_Sib,~]    = sort(t_stats_DZ_Sib);
+        ctl_val_t_stats_DZ_Sib = st_stats_DZ_Sib(ctl_val_index);
+        [f1,x1]                = hist(st_stats_DZ_Sib,100);
+        n_t_stats_DZ_Sib       = min(find(st_stats_DZ_Sib(:)==t_stats_DZ_Sib(N)));
+        bar(x1,f1/N);
+        
+        % Calculate the permutation-based p-value
+        p_t_stats_DZ_Sib = (N-n_t_stats_DZ_Sib+1)/N;
+        
+        title(sprintf('H0 dist of two-sample t-statistic for rDZ & rSib, P-value=%.3f',p_t_stats_DZ_Sib));
+        xlabel('t-statistic for rDZ vs. rSib');
+        ylabel('frequency');
+        yLimits = get(gca,'YLim');
+        line([t_stats_DZ_Sib(N) t_stats_DZ_Sib(N)],[0 yLimits(2)],'Marker','.','Color','green');
+        line([ctl_val_t_stats_DZ_Sib ctl_val_t_stats_DZ_Sib],[0 yLimits(2)],'Marker','.','LineStyle','-.','Color','red');
+        text(t_stats_DZ_Sib(N),0.8*yLimits(2),num2str(t_stats_DZ_Sib(N)))
+        text(ctl_val_t_stats_DZ_Sib,0.9*yLimits(2),num2str(ctl_val_t_stats_DZ_Sib))
+        print('-dpdf',fullfile(ACEfit_Par.ResDir,['H0dist_Tstat_rDZ_rSib' ResSuf '.pdf']));
+        
+        %
+        % 2nd DZ vs Sib statistic: mean difference
+        %
+        f = f+1;
+        SetFig(f);
+        [sCorrDiff_DZ_Sib,~]    = sort(CorrDiff_DZ_Sib);
+        ctl_val_CorrDiff_DZ_Sib = sCorrDiff_DZ_Sib(ctl_val_index);
+        [f2,x2]                 = hist(sCorrDiff_DZ_Sib,100);
+        n_CorrDiff_DZ_Sib       = min(find(sCorrDiff_DZ_Sib(:)==CorrDiff_DZ_Sib(N)));
+        bar(x2,f2/N);
+        
+        % Calculate the permutation-based p-value
+        p_CorrDiff_DZ_Sib = (N-n_CorrDiff_DZ_Sib+1)/N;
+        
+        title(sprintf('H0 dist of mean difference between rDZ & rSib, P-value=%.3f',p_CorrDiff_DZ_Sib));
+        xlabel('E(rDZ) - E(rSib)');
+        ylabel('frequency');
+        yLimits = get(gca,'YLim');
+        line([CorrDiff_DZ_Sib(N) CorrDiff_DZ_Sib(N)],[0 yLimits(2)],'Marker','.','Color','green');
+        line([ctl_val_CorrDiff_DZ_Sib ctl_val_CorrDiff_DZ_Sib],[0 yLimits(2)],'Marker','.','LineStyle','-.','Color','red');
+        text(CorrDiff_DZ_Sib(N),0.8*yLimits(2),num2str(CorrDiff_DZ_Sib(N)))
+        text(ctl_val_CorrDiff_DZ_Sib,0.9*yLimits(2),num2str(ctl_val_CorrDiff_DZ_Sib))
+        print('-dpdf',fullfile(ACEfit_Par.ResDir,['H0dist_Diff_rDZ_rSib' ResSuf '.pdf']));
+        
+        Pvals_DZSib = [p_t_stats_DZ_Sib p_CorrDiff_DZ_Sib]';
+        
+        % Save p-values
+        save(fullfile(ACEfit_Par.ResDir,['Pvals_AgHe' ResSuf]),'Pvals_MZDZ','Pvals_DZSib');
+    else
+        save(fullfile(ACEfit_Par.ResDir,['Pvals_AgHe' ResSuf]),'Pvals_MZDZ');
+    end
     
 end
 
@@ -360,14 +389,8 @@ if ACEfit_Par.nBoot>0
     Boot_rMZ1  = zeros(nB,nMZF);
     Boot_rDZ1  = zeros(nB,nDZF);
     
-    Boot_rDZ2  = zeros(nB,nDZF);
-    Boot_rSib2 = zeros(nB,nSibP);
-    
     Boot_rMZ1(end,:)  = rMZo;
     Boot_rDZ1(end,:)  = rDZo;
-    
-    Boot_rDZ2(end,:)  = rDZo;
-    Boot_rSib2(end,:) = rSibo;
     
     % Compute rMZ & rDZ for all bootstrap replicates
     Boot_MZDZ = ACEfit_Par.Boot_index(:,1:nMZF+nDZF);
@@ -377,26 +400,41 @@ if ACEfit_Par.nBoot>0
         Boot_rDZ1(i,:)  = Boot_rDZ1(end,Boot_label(nMZF+[1:nDZF])-nMZF);
     end
     
-    % Compute rDZ & rSib for all bootstrap replicates
-    for i=1:ACEfit_Par.nBoot
-        Boot_label      = Boot_DZSib(i,:);
-        Boot_rDZ2(i,:)  = Boot_rDZ2(end,Boot_label(1:nDZF));
-        Boot_rSib2(i,:) = Boot_rSib2(end,Boot_label(nDZF+[1:nSibP])-nDZF);
-    end
-    
-    % Bootstrapping mean differences
+    % Bootstrapping MZ vs DZ mean differences
     Boot_diff_MZ_DZ  = mean(Boot_rMZ1,2)-mean(Boot_rDZ1,2);
-    Boot_diff_DZ_Sib = mean(Boot_rDZ2,2)-mean(Boot_rSib2,2);
     
     % Confidence level 1-Balpha
     CI_MZDZ  = zeros(1,2);
-    CI_DZSib = zeros(1,2);
+    [CI_MZDZ(1), CI_MZDZ(2)] = Bootstrap_CI(Boot_diff_MZ_DZ, Balpha);
     
-    [CI_MZDZ(1),  CI_MZDZ(2) ] = Bootstrap_CI(Boot_diff_MZ_DZ,  Balpha);
-    [CI_DZSib(1), CI_DZSib(2)] = Bootstrap_CI(Boot_diff_DZ_Sib, Balpha);
-    
-    save(fullfile(ACEfit_Par.ResDir,['CIs_AgHe' ResSuf]),'CI_MZDZ','CI_DZSib');
-    
+    if nSibP>0
+        Boot_rDZ2  = zeros(nB,nDZF);
+        Boot_rSib2 = zeros(nB,nSibP);
+        
+        Boot_rDZ2(end,:)  = rDZo;
+        Boot_rSib2(end,:) = rSibo;
+        
+        % Compute rDZ & rSib for all bootstrap replicates
+        for i=1:ACEfit_Par.nBoot
+            Boot_label      = Boot_DZSib(i,:);
+            Boot_rDZ2(i,:)  = Boot_rDZ2(end,Boot_label(1:nDZF));
+            Boot_rSib2(i,:) = Boot_rSib2(end,Boot_label(nDZF+[1:nSibP])-nDZF);
+        end
+        
+        % Bootstrapping DZ vs Sib mean differences
+        Boot_diff_DZ_Sib = mean(Boot_rDZ2,2)-mean(Boot_rSib2,2);
+        
+        % Confidence level 1-Balpha
+        CI_DZSib = zeros(1,2);
+        [CI_DZSib(1), CI_DZSib(2)] = Bootstrap_CI(Boot_diff_DZ_Sib, Balpha);
+        
+        % Save CI's
+        save(fullfile(ACEfit_Par.ResDir,['CIs_AgHe' ResSuf]),'CI_MZDZ','CI_DZSib');
+        
+    else
+        save(fullfile(ACEfit_Par.ResDir,['CIs_AgHe' ResSuf]),'CI_MZDZ');
+    end
+
 end
 
 
